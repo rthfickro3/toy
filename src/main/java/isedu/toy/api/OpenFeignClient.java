f0c073fc44475a9447fa1c9ba9520c8f48ce1376
rthfickro3 <rthfickro3@naver.com>
2022-05-09 15:19:36 +0900

    RequestLine

--- src/main/java/isedu/toy/api/OpenFeignClient.java
+++ src/main/java/isedu/toy/api/OpenFeignClient.java
@@ -1,6 +1,8 @@
 package isedu.toy.api;
 
+import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,4 +11,7 @@ public interface OpenFeignClient {
 
     @GetMapping("/feign/msg")
     String getMessage(@RequestParam("param") String val);
+
+/*    @RequestLine("GET /feign/msg")
+    String getMessage(@RequestParam("param") String val);*/
 }
---
+++ src/main/java/isedu/toy/config/FeignConfiguration.java
@@ -0,0 +1,14 @@
+package isedu.toy.config;
+
+import feign.Contract;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfiguration {
+
+/*    @Bean
+    public Contract useFeignAnnotation(){
+        return new Contract.Default();
+    }*/
+}
